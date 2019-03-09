99d47d970cb83a2c2ecc6aab625f5c55edf99c0d
Taron Abgaryan <taronabg@mail.ru>
2019-03-09 20:51:44 +0400

    kendoGridModule

---
+++ index.js
@@ -0,0 +1,47 @@
+import * as $ from "jquery";
+require ("@progress/kendo-ui/js/kendo.grid");
+
+export function createGrid(rootElement, cars) {
+
+    var div = document.createElement('div');
+    rootElement.appendChild(div);
+    $("div").addClass("grid");
+    $("div").kendoGrid({
+        columns: [
+        { title: "id", field: "id" },
+        { title: "Model", field: "model" },
+        { title: "Price", field: "price" },
+        { title: "Country", field: "country" },
+        {
+            title: "img",
+            field: "picture",
+            template: '<img src="#= picture #" alt="image"  class="img"/>',
+        },
+        { command: "edit" }
+        ],
+        dataSource: {
+        data: cars,
+        pageSize: 5,
+        schema: {
+            model: {
+            id: "id",
+            fields: {
+                "id": { type: "number" }
+            }
+            }
+        }
+        },
+        pageable: true,
+        sortable: { mode: "multiple" },
+        groupable: true,
+        selectable: true,
+        editable: "popup",
+
+        edit: function (e) {
+        if (!e.model.isNew()) {
+            var numeric = e.container.find("input[name=id]").data("kendoNumericTextBox");
+            numeric.enable(false);
+        }
+        }
+    });
+}
